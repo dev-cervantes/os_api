5c8f147d0a100d0fb78d66f01dee7a4fd430ddf2
Gabriel de Matos Hainzenreder <gabrielmatos1999@rede.ulbra.br>
2022-12-03 14:21:00 -0300

    Criado rota para buscar por código da OS

--- app/Http/Controllers/OsController.php
+++ app/Http/Controllers/OsController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Os;
+use App\Models\OsEquipamentoItem;
+use App\Models\OsProduto;
+use App\Models\OsServico;
+use App\Models\OsUsuarioResponsavel;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OsController extends Controller
 {
@@ -14,7 +19,7 @@ class OsController extends Controller
         try {
             $body = $request->all();
 
-            $validate = $this->validator($body, $this->rules(), $this->messages());
+            $validate = $this->validator($body, $this->rulesFiltros(), $this->messagesFiltros());
             if ($validate->fails()) {
                 throw new Exception($validate->errors()->first(), 422);
             }
@@ -104,7 +109,87 @@ class OsController extends Controller
         return $this->sendResponse([]);
     }
 
+    public function getByCodigoOs(int $codigo): JsonResponse
+    {
+        try {
+            $os = Os::query()->allRelations()->where("os_codigo", "=", "$codigo")->first();
+
+            if (is_null($os))
+                return $this->sendResponseError("OS não encontrada.");
+
+            $os->responsavel = OsUsuarioResponsavel::getResponsavel($os->id_os);
+
+            return $this->sendResponse($os);
+        } catch (Exception $e) {
+            return $this->sendResponseError($e->getMessage(), $e->getCode());
+        }
+    }
+
     protected function rules(): array
+    {
+        return [
+            "id_os_situacao" => "integer|required",
+            "id_os_tipo_atendimento" => "integer|required",
+            "id_cliente" => "integer|required",
+            "id_usuario_atendente" => "integer|required",
+            "id_usuario_aprovacao" => "integer|required",
+            "id_usuario_encerramento" => "integer|nullable",
+            "os_codigo" => "integer|nullable",
+            "data_hora" => "date_format:Y-m-d H:i:s|required",
+            "data_hora_previsao_entrega" => "date_format:Y-m-d H:i:s|nullable",
+            "data_hora_entrega" => "date_format:Y-m-d H:i:s|nullable",
+            "data_hora_aprovacao" => "date_format:Y-m-d H:i:s|nullable",
+            "data_hora_encerramento" => "date_format:Y-m-d H:i:s|nullable",
+            "obs" => "string|nullable",
+            "inativo" => "boolean",
+            "equipamentos_itens" => "array",
+            "equipamentos_itens.*.id_equipamento_item" => "integer|required",
+            "equipamentos_itens.*.problema_reclamado" => "string|nullable",
+            "equipamentos_itens.*.problema_constatado" => "string|nullable",
+            "equipamentos_itens.*.obs" => "string|nullable",
+			"equipamentos_itens.*.servicos" => "array",
+			"equipamentos_itens.*.produtos" => "array",
+            "equipamentos_itens.*.servicos.*.id_servico" => "integer|required",
+            "equipamentos_itens.*.servicos.*.qtd" => "numeric|required",
+            "equipamentos_itens.*.servicos.*.id_usuario" => "integer|required",
+            "equipamentos_itens.*.produtos.*.id_produto" => "integer|required",
+            "equipamentos_itens.*.produtos.*.qtd" => "numeric|required",
+        ];
+    }
+
+    protected function messages()
+    {
+        return [
+            "id_os_situacao.integer" => "Situação inválida.",
+            "id_os_situacao" => "Situação não informada.",
+            "id_os_tipo_atendimento.integer" => "Tipo de atendimento inválido.",
+            "id_os_tipo_atendimento.required" => "Tipo de atendimento não informado.",
+            "id_cliente.required" => "Cliente não informado.",
+            "id_cliente.integer" => "Cliente inválido.",
+            "id_usuario_atendente.integer" => "Usuário atendente inválido.",
+            "id_usuario_atendente.required" => "Usuário atendente não informado.",
+            "id_usuario_aprovacao.integer" => "Usuário aprovação inválido.",
+            "id_usuario_aprovacao.required" => "Usuário aprovação não informado.",
+            "id_usuario_encerramento.integer" => "Usuário encerramento inválido.",
+            "os_codigo.integer" => "Código da OS é inválida.",
+            "data_hora.date_format" => "Data inválida.",
+            "data_hora.required" => "Data não informada.",
+            "data_hora_previsao_entrega" => "Data inválida.",
+            "data_hora_entrega" => "Data inválida.",
+            "data_hora_aprovacao" => "Data inválida.",
+            "data_hora_encerramento" => "Data inválida.",
+            "obs" => "Observação inválida.",
+            "equipamentos_itens.*.id_equipamento_item.integer" => "Identificador inválido.",
+            "equipamentos_itens.*.id_equipamento_item.required" => "Identificador não informado.",
+            "equipamentos_itens.*.servicos.*.id_servico.required" => "Serviço não informado.",
+            "equipamentos_itens.*.servicos.*.qtd.required" => "Quantidade não informada para o serviço.",
+            "equipamentos_itens.*.servicos.*.id_usuario.required" => "Usuário não informado para o serviço.",
+            "equipamentos_itens.*.produtos.*.id_produto.required" => "Produto não informado.",
+            "equipamentos_itens.*.produtos.*.qtd.required" => "Quantidade não informada para o produto.",
+        ];
+    }
+
+    protected function rulesFiltros(): array
     {
         return [
             "data_inicial" => "date_format:Y-m-d|nullable",
@@ -117,7 +202,7 @@ class OsController extends Controller
         ];
     }
 
-    protected function messages()
+    protected function messagesFiltros()
     {
         return [
             "date_format" => "Data é inválida.",
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\OsController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("os", OsController::class);
+Route::get("os/codigo/{codigo}", [OsController::class, "getByCodigoOs"]);
