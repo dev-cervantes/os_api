b81bf8c0fe29513f006f5b0890f20bb800ce4da7
Gabriel de Matos Hainzenreder <gabrielmatos1999@rede.ulbra.br>
2022-12-06 23:38:38 -0300

    Criado middleware para validar secret nos headers

---
+++ app/Http/Middleware/ValidateSecretIsValid.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ValidateSecretIsValid
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $headers = $request->header();
+
+        if (!isset($headers['secret']) || $headers['secret'][0] != env('APP_SECRET')) {
+            abort(403, "Sem acesso");
+        }
+
+        return $next($request);
+    }
+}
