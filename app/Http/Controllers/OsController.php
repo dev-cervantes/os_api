22cd44bac454ed7a85ed3d5d3d89a9741c1b0e83
Christian Miranda da Costa <110126493+christian-mcosta@users.noreply.github.com>
2024-04-29 10:14:19 -0300

    Alterar as OS's ao liberar uma versão

--- app/Http/Controllers/OsController.php
+++ app/Http/Controllers/OsController.php
@@ -12,8 +12,6 @@ use App\Models\Os;
 use App\Models\OsEquipamentoItem;
 use App\Models\OsProduto;
 use App\Models\OsServico;
-use App\Models\OsSituacao;
-use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -23,6 +21,11 @@ use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 class OsController extends Controller
 {
     const PAGINATOR_PER_PAGE_DEFAULT = 100;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['liberar']]);
+    }
 
     public function index(FilterRequest $request): OsCollectionResource
     {
@@ -230,6 +233,69 @@ class OsController extends Controller
         return new OsResource(
             resource: Os::allRelations()->find($id)
         );
+    }
+
+    public function liberar(Request $request): JsonResponse
+    {   //Valida e garante os dados recebidos pela requisição HTTP
+        $request->validate(['oss' => 'required|array', 'versao' => 'required|string', 'aplicativo' => 'required|string']);
+
+        //retira o "OS" e espaços das Oss e converte para int
+        $reqOss = array();
+
+        foreach ($request->get('oss') as $os) {
+            $osInt = trim(str_replace("OS", "", $os));
+            array_push($reqOss, (int)$osInt);
+        }
+
+        //Busca todas as OS
+        $OSs = Os::query()->whereIn('os_codigo', $reqOss)->get();
+
+        //Inicia uma transação
+        DB::beginTransaction();
+
+        // Obtém a versão diretamente do request
+        $versao = $request->get('versao');
+
+
+        $OSs->each(function ($os) use ($request, $versao) {
+
+            //Faz as mudanças na OS
+            switch ($os->id_os_tipo_atendimento) {
+
+                // tipo de atendimento 1 - novo cliente/novo piloto/novo parceiro
+                case 29:
+                case 36:
+                case 37:
+                    $os->id_os_situacao = 12;
+                    break;
+
+                // tipo de atendimento 2 - bug/bug critico
+                case 30:
+                case 39:
+                    $os->id_os_situacao = 26;
+                    break;
+
+                // tipo de atendimento 4 - consultoria tributaria
+                case 32:
+                    $os->id_os_situacao = 33;
+                    break;
+            }
+            $os->id_usuario_responsavel = 25;
+
+            // Atualiza o problema_constatado na tabela OsEquipamentoItem
+            $os->equipamentosItens->each(function ($item) use ($versao, $request) {
+                $item->problema_constatado = "Contemplada na versão {$versao} do {$request->get('aplicativo')}\n{$item->problema_constatado}";
+                $item->save();
+            });
+
+            //Salva a OS
+            $os->save();
+        });
+
+        DB::commit();
+
+        //Retorna vazio... o retorno no status 200 vai garantir sucesso.
+        return response()->json([]);
     }
 
     public function destroy(int $id): JsonResponse
